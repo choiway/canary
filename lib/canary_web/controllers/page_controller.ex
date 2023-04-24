86ebfd2968bbb535bdec9c8cec0921f1aa47223f
Wayne Choi <waynechoi@gmail.com>
2023-04-23 22:26:15 -0700

    Add machines to home page

--- lib/canary_web/controllers/page_controller.ex
+++ lib/canary_web/controllers/page_controller.ex
@@ -1,9 +1,13 @@
 defmodule CanaryWeb.PageController do
   use CanaryWeb, :controller
 
+  alias Canary.Machines
+  # alias Canary.Machines.Machine
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    machines = Machines.list_machines()
+    render(conn, :home, layout: false, machines: machines)
   end
 end
